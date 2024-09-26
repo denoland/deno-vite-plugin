96adfcff7eb3eae62fb343dc045e53fc97ee3846
Marvin Hagemeister <marvin@deno.com>
2024-09-26 15:10:37 +0200

    fix: node esm (#10)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Plugin } from "vite";
-import prefixPlugin from "./prefixPlugin";
-import mainPlugin from "./resolvePlugin";
-import { DenoResolveResult } from "./resolver";
+import prefixPlugin from "./prefixPlugin.js";
+import mainPlugin from "./resolvePlugin.js";
+import { DenoResolveResult } from "./resolver.js";
 
 export default function denoPlugin(): Plugin[] {
   const cache = new Map<string, DenoResolveResult>();
